8aeee2183bd46eac49c331e845bbae5d2d10d1cc
Nelson <nelsoncc1812@gmail.com>
2020-01-19 18:24:28 +0000

    I have changed the Math.round method with the -1, for the Math.floor :D

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -26,7 +26,6 @@ class Viking extends Soldier {
         if (this.health > 0) return `${this.name} has received ${damage} points of damage`
         else return `${this.name} has died in act of combat`
     }
-
     battleCry() {
         return "Odin Owns You All!"
     }
@@ -59,24 +58,24 @@ class War {
         this.saxonArmy.push(saxonObject)
     }
     vikingAttack() {
-        let randomSaxon = Math.round(Math.random() * (this.saxonArmy.length - 1))
+        let randomSaxon = Math.floor(Math.random() * (this.saxonArmy.length))
         let result = this.saxonArmy[randomSaxon].receiveDamage(this.vikingArmy[Math.round(Math.random() * (this.vikingArmy.length - 1))].strength)
         if (this.saxonArmy[randomSaxon].health <= 0) this.saxonArmy.splice(randomSaxon, 1)
 
         return result
     }
     saxonAttack() {
-      let randomViking = Math.round(Math.random() * (this.vikingArmy.length - 1))
-      let result = this.vikingArmy[randomViking].receiveDamage(this.saxonArmy[Math.round(Math.random() * (this.saxonArmy.length - 1))].strength)
-      if (this.vikingArmy[randomViking].health <= 0) this.vikingArmy.splice(randomViking, 1)
+        let randomViking = Math.floor(Math.random() * (this.vikingArmy.length))
+        let result = this.vikingArmy[randomViking].receiveDamage(this.saxonArmy[Math.round(Math.random() * (this.saxonArmy.length - 1))].strength)
+        if (this.vikingArmy[randomViking].health <= 0) this.vikingArmy.splice(randomViking, 1)
+
         return result
     }
     showStatus() {
 
-        if(this.saxonArmy.length==0)      return "Vikings have won the war of the century!"
-        else if(this.vikingArmy.length==0)    return "Saxons have fought for their lives and survived another day..."
-        else                        return  "Vikings and Saxons are still in the thick of battle."
-        
+        if (this.saxonArmy.length == 0) return "Vikings have won the war of the century!"
+        else if (this.vikingArmy.length == 0) return "Saxons have fought for their lives and survived another day..."
+        else return "Vikings and Saxons are still in the thick of battle."
 
     }
 
